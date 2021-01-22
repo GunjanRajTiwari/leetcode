ac47d8d96ba190e59eee147cbac2f7e9c31d25cf
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-01-22 17:33:33 +0545

    i: problem 938

---
+++ 938.cpp
@@ -0,0 +1,23 @@
+// Range sum of BST
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (!root) return 0;
+        int sum = rangeSumBST(root->left,low,high);
+        if(root->val>=low && root->val <= high) sum+=root->val;
+        sum+=rangeSumBST(root->right,low,high);
+        return sum;
+    }
+};
