0c710223c8de86a237135d04fe340ca39e3086fc
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-02-08 09:31:38 +0545

    i: problem 94

---
+++ 94.cpp
@@ -0,0 +1,32 @@
+// Binary Tree Inorder Traversal
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+  vector<int> ans;
+  Solution()
+  {
+    ans.reserve(100);
+  }
+
+  vector<int> inorderTraversal(TreeNode *root)
+  {
+    if (!root)
+      return ans;
+    inorderTraversal(root->left);
+    ans.push_back(root->val);
+    inorderTraversal(root->right);
+    return ans;
+  }
+};
